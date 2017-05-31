46168c47ce758b01067f0b3f51e458e38c8aba7c
Rails Udemy Course <alwyncheong@gmail.com>
2017-05-31 14:12:10 +0000

    Update current_user to be article creator

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,7 +23,7 @@ class ArticlesController < ApplicationController
     #@article.save
     #redirect_to article_path(@article)
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       #do something
       flash[:success] = "Article was sucessfully created"
